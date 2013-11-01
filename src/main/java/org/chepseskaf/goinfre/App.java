99055ce57d00d79aae502988334f15db2624191b
Sokaris Mykerinos <chepseskaf@gmail.com>
2013-11-01 14:29:06 +0100

    create minimal interface

---
+++ src/main/java/org/chepseskaf/goinfre/App.java
@@ -0,0 +1,18 @@
+package org.chepseskaf.goinfre;
+
+import org.chepseskaf.goinfre.dispatcher.api.Consumer;
+import org.chepseskaf.goinfre.dispatcher.api.Producer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+    }
+}
---
+++ src/main/java/org/chepseskaf/goinfre/dispatcher/api/Consumer.java
@@ -0,0 +1,8 @@
+package org.chepseskaf.goinfre.dispatcher.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chepseskaf
+ */
+public interface Consumer {
+}
